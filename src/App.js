7ce6ed752b1e1ca0fa0e14759ede8975657bb60c
fotomain <foto888999@gmail.com>
2023-01-06 16:39:49 +0200

    CSS

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Row1Search from "./comp/Row1Search";
 import Home1AnimeTextCircle from "./comp/Home1AnimeTextCircle";
+import InfiniteScrollModel from "./comp/InfiniteScrollModel";
+import ReactVirtualized from "./comp/ReactVirtualized";
 
 // npm start
 
 // npm i --save @fortawesome/fontawesome-svg-core
 // npm install --save @fortawesome/free-solid-svg-icons
 // npm install --save @fortawesome/react-fontawesome
-// npm i react-infinite-scroller
+// npm install react-virtualized --save
+// npm install --save-dev faker@5.5.3
 
 function App() {
   return (
     <div className="App">
 
-
-        <Row1Search />
+        <ReactVirtualized />
+        {/*<InfiniteScrollModel />*/}
+        {/*<Row1Search />*/}
         {/*<Home1AnimeTextCircle />*/}
 
     </div>
---
+++ src/comp/ReactVirtualized.js
@@ -0,0 +1,82 @@
+﻿import React, {useEffect, useRef, useState} from 'react';
+
+import faker from 'faker'
+import { List } from "react-virtualized";
+
+function ReactVirtualized () {
+
+    const data000 = new Array(1000).fill().map((value, index) => ({
+        id: index,
+        name: faker.name.firstName(5),
+        body: faker.lorem.paragraph(1),
+    }));
+
+    const [data_list, set_data_list] = useState(new Array(1000).fill().map((value, index) => ({
+        id: index,
+        name: faker.name.firstName(5),
+        body: faker.lorem.paragraph(1).substring(1,40),
+    })));
+    const itemsPerPage = 20;
+    const [hasMore, setHasMore] = useState(true);
+    const [records, setrecords] = useState(itemsPerPage);
+
+    const renderRow = ({ index, key, style }) => (
+        // style={{paddingTop: '20px', paddingBottom: '20px'}}
+
+            <div  key={key} style={{...style,paddingTop: '20px'}} className="post">
+                <h5>{`${data_list[index].name}-${data_list[index].id}`}</h5>
+
+                <p>{data_list[index].body}</p>
+
+            </div>
+
+
+    )
+
+    const tableRef = useRef();
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    useEffect(
+        () => {
+            if(tableRef) {
+                tableRef.current.recomputeRowHeights();
+            }
+        },
+        [selectedIndex]
+    );
+
+    return(
+
+    <div className={'main_grid'}>
+        <button onClick={(e)=>{
+            console.log(111)
+
+            const t_index = Date.now()
+            data_list.unshift(
+            {
+                id: t_index,
+                name: faker.name.firstName(5),
+                body: faker.lorem.paragraph(1).substring(1,40),            })
+            set_data_list(data_list)
+            setSelectedIndex(t_index)
+        }}>Add____Up</button>
+        <button>Add__Down</button>
+        {/*<section style={{ overflow: 'scroll', width:'80%', height:'600px', display: 'grid', gap: '2rem' }}>*/}
+
+
+            <List
+                ref={tableRef}
+                width={300}
+                height={500}
+                rowRenderer={renderRow}
+                rowCount={data_list.length}
+                rowHeight={120}
+            />
+        {/*</section>*/}
+
+    </div> // return
+
+)
+
+}
+export default ReactVirtualized
